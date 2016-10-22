561255b9c8e37d4295681783eaf2f2b39f45e68a
owen <owenwassell@Owens-MacBook-Pro.local>
2016-10-22 12:08:08 +0100

    adds church to play around with

---
+++ 360-church/README.md
@@ -0,0 +1,6 @@
+# 360 Web VR demo of the inside of a church
+
+## Dependencies
+
+- [A-Frame](https://github.com/aframevr/aframe)
+- [K-Frame](https://github.com/ngokevin/k-frame)
---
+++ 360-church/components/funcs.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+document.getElementById("oneSub").onclick = function() { 
+	alert("testString");
+	// var element = document.getElementById("mainScene"); 
+	// element.setAttribute("visible", "false")
+};
